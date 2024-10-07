c2af8c975301f655a810e60f20579883192ea793
Natan Marins <natanmarins66@gmail.com>
2024-10-07 18:00:46 -0300

    alteração no front

--- resources/views/holding/show.blade.php
+++ resources/views/holding/show.blade.php
@@ -2,50 +2,118 @@
 
 @section('content')
 
-<div class="container mt-5">
-    <h2 class="text-center mb-4">Detalhes da Holding: {{ $holding->nome }}</h2>
+    <div class="col-md-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <!-- topo da página -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="d-flex flex-column flex-md-row justify-content-between align-items-center py-2">
+                            <h2 class="text-center mb-4">Detalhes da Holding - <strong> {{ $holding->nome }} </strong></h2>
+                        </div>
+                    </div>
+                </div>
+                <!-- topo da página -->
 
-    <!-- Detalhes da holding -->
-    <div class="mb-4">
-        <h4>Dados da Holding:</h4>
-        <p><strong>CNPJ:</strong> {{ $holding->cnpj }}</p>
-        <p><strong>Endereço:</strong> {{ $holding->endereco }}</p>
-        <p><strong>Telefone:</strong> {{ $holding->telefone }}</p>
-        <p><strong>Email:</strong> {{ $holding->email }}</p>
+                <div class="row">
+                    <div class="col-md-4">
+                        <address>
+                            <p class="fw-bold"> CNPJ: </p>
+                            <p class="mb-2">  {{ $holding->cnpj }} </p>
+                            <p class="fw-bold"> Telefone: </p>
+                            <p> {{ $holding->telefone }} </p>
+                        </address>
+                    </div>
+                    <div class="col-md-4">
+                        <address>
+                            <p class="fw-bold"> E-mail: </p>
+                            <p> {{ $holding->email }} </p>
+                        </address>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <address>
+                            <p class="fw-bold">  Endereço: </p>
+                            <p>{{ $holding->endereco }}</p>
+                        </address>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
     </div>
+    <div class="col-md-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <!-- topo da página -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="d-flex flex-column flex-md-row justify-content-between align-items-center py-2">
+                            <h4 class="text-center mb-4">Empresas cadastradas nesta Holding</h4>
+                            <a href="{{ route('holding.create') }}" class="btn btn-inverse-info btn-sm"title="Cadastrar Nova Holding">
+                                <i class="fa-solid fa-plus btn-icon-prepend"></i>
+                                Nova Holding
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- topo da página -->
+                
+                    <!-- Verificar se há empresas cadastradas -->
+                    @if($holding->empresas->isEmpty())
+                        <!-- alerta -->
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <p class="card-description">
+                                Não há empresas cadastradas nesta holding.
+                            </p>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        <!-- alerta -->
+                    @else
 
-    <!-- Lista de empresas associadas à holding -->
-    <div>
-        <h4>Empresas cadastradas nesta Holding:</h4>
+                    <div class="table-responsive pt-3">
+                        <table class="table table-striped project-orders-table table-hover">
+                            <thead>
+                                <tr>
+                                    <th class="ms-5">ID</th>
+                                    <th>Nome</th>
+                                    <th>CNPJ</th>
+                                    <th>Endereço</th>
+                                    <th>Telefone</th>
+                                    <th style="width: 100px;">Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($holding->empresas as $empresa)
+                                <tr>
+                                    <tr>
+                                        <td>{{ $empresa->id }}</td>
+                                        <td>{{ $empresa->nome }}</td>
+                                        <td>{{ $empresa->cnpj }}</td>
+                                        <td>{{ $empresa->endereco }}</td>
+                                        <td>{{ $empresa->telefone }}</td>
+                                        <td>
+                                            <div class="d-flex align-items-center">
+                                                <a href="{{ route('holding.edit', $holding->id) }}" class="btn btn-inverse-info btn-sm btn-icon-text me-3" title="Editar Holding">
+                                                    <i class="fa-solid fa-edit btn-icon-append"></i>                          
+                                                </a>
+                                                <a href="{{ route('holding.show', $holding->id) }}" class="btn btn-inverse-info btn-sm btn-icon-text  me-3" title="Detalhes">
+                                                    <i class="fa-regular fa-eye"></i>
+                                                </a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+                </div>
 
-        <!-- Verificar se há empresas cadastradas -->
-        @if($holding->empresas->isEmpty())
-            <p>Não há empresas cadastradas nesta holding.</p>
-        @else
-            <table style="width: 100%; border-collapse: collapse; border: 1px solid #ddd;">
-                <thead>
-                    <tr>
-                        <th style="border: 1px solid #ddd; padding: 12px;">ID</th>
-                        <th style="border: 1px solid #ddd; padding: 12px;">Nome</th>
-                        <th style="border: 1px solid #ddd; padding: 12px;">CNPJ</th>
-                        <th style="border: 1px solid #ddd; padding: 12px;">Endereço</th>
-                        <th style="border: 1px solid #ddd; padding: 12px;">Telefone</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($holding->empresas as $empresa)
-                    <tr>
-                        <td style="border: 1px solid #ddd; padding: 12px;">{{ $empresa->id }}</td>
-                        <td style="border: 1px solid #ddd; padding: 12px;">{{ $empresa->nome }}</td>
-                        <td style="border: 1px solid #ddd; padding: 12px;">{{ $empresa->cnpj }}</td>
-                        <td style="border: 1px solid #ddd; padding: 12px;">{{ $empresa->endereco }}</td>
-                        <td style="border: 1px solid #ddd; padding: 12px;">{{ $empresa->telefone }}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        @endif
+            </div>
+        </div>
     </div>
-</div>
 
 @endsection
